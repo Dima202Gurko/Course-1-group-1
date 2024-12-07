eb550d7b2ef0e055b5ec97a9dddb07524144aa92
Dima202Gurko <dmitrijgurko38@gmail.com>
2024-12-07 21:36:52 +0900

    Выполнил практику A23

---
+++ Course/1st year/Lesson5/PracticeA-B/Practic2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._11._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Практика A2
+            int[] num = { 2, 3, 1, 3, 2, 4};
+            Array.Sort(num);
+            int a = 0;
+            int b = 0;
+            for(int i = 0; i < num.Length; i++)
+            {
+                for(int j = 0; j < num.Length;  j++)
+                {
+                    if (num[i] == num[j])
+                    {
+                        a++;                       
+                    }
+                    if (a == 1 && j == num.Length-1)
+                    {
+                        b++;
+                    }
+                    if(j == num.Length - 1)
+                    {
+                        a = 0;
+                    }
+                }
+            }
+            foreach(int i in num)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine($"\nКоличество уникальных элементов:{b}");
+        }
+    }
+}
---
+++ Course/1st year/Lesson5/PracticeA-B/Practic3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._11._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Практика A3
+            int[] num = { 1, 2, 1, 4, 4, 1, 6, 7, 7, 7, 8 };
+            Array.Sort(num);
+            int[] copynum = new int[num.Length + 1];
+            int max = 0;
+            for(int i = 0; i < num.Length; i++)
+            {
+                int a = 0;
+                if (i == 0 || num[i] != num[i - 1])
+                {
+                    for (int j = 0; j < num.Length; j++)
+                    {
+                        if (num[j] == num[i])
+                        {
+                            a++;
+                        }
+                        copynum[i + 1] = a;
+                        if(max < a)
+                        {
+                            max = a;
+                        }
+                    }
+                }               
+            }
+            for(int i = 0; i < copynum.Length; i++)
+            {
+                if (copynum[i] == max)
+                {
+                    Console.WriteLine($"{i}: {copynum[i]}");
+                }
+            }          
+        }
+    }
+}
